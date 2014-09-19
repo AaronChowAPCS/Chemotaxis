ac021a4d9f80915af1b13b076aa20ed4aa92cf1d
aaronchowapcs <aahchow@gmail.com>
2014-09-19 10:52:28 -0700

    added firsrt name to index.html

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 
 int colorA = color(150,150,150);
 int colorB;
@@ -5,7 +21,7 @@ boolean eaten = false;
 boolean foodsies = true;
 Bacteria [] pest;
 Food yummy;
-void setup()   
+public void setup()   
 {     
  	frameRate(15);
  	size(500,500);
@@ -16,7 +32,7 @@ void setup()
  	}
  	yummy = new Food();
  }   
-void draw()   
+public void draw()   
 {   
 	background(255);
 	if(foodsies == true)
@@ -35,7 +51,7 @@ void draw()
  				mousePressed();
  			}
 }  
-void mousePressed() //Creates a new food with random color and location 
+public void mousePressed() //Creates a new food with random color and location 
 {
 	yummy = new Food();
 	yummy.show();
@@ -51,7 +67,7 @@ class Food
    		foodX = (int)(Math.random()*450);
    		foodY = (int)(Math.random()*450);
     }
-	void show()
+	public void show()
 	{
    		noStroke();
    		fill(colorB);
@@ -69,18 +85,18 @@ class Bacteria
  		myX = x;
  		myY = y; 
 		}
- 	void move()
+ 	public void move()
  	{
  		myX = myX + (int)(Math.random() * 8) - 4;
  		myY = myY + (int)(Math.random() * 8) - 4;
  	}
- 	void show()
+ 	public void show()
  	{
  		noStroke();
  		fill(colorA);
  		ellipse(myX,myY,15,15);
  	}
- 	void chase() //Causes the bacteria to hunt the food
+ 	public void chase() //Causes the bacteria to hunt the food
  	{
  		if(yummy.foodX > myX)
  		{
@@ -100,3 +116,12 @@ class Bacteria
  		}
  	}
 }   
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
