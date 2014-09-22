8f9c6e27babbe20aee2887f715d1912bd52641cb
aaronchowapcs <aahchow@gmail.com>
2014-09-21 17:15:20 -0700

    Added first name to index.html

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,73 +1,17 @@
-
-int colorA = color(150,150,150);
-int colorB;
 boolean eaten = false;
 boolean foodsies = true;
 Bacteria [] pest;
 Food yummy;
-void setup()   
-{     
- 	frameRate(15);
- 	size(500,500);
- 	pest = new Bacteria[5];
- 	for(int index = 0; index < pest.length; index++)
- 	{
- 		 pest[index] = new Bacteria(250,250);
- 	}
- 	yummy = new Food();
- }   
-void draw()   
-{   
-	background(255);
-	if(foodsies == true)
-		yummy.show();
-	for(int index = 0; index < pest.length; index++)
-	{
-		pest[index].move();
- 		pest[index].show();
- 		pest[index].chase();
-	}
-	 			if((myX > yummy.foodX - 20 && myX < yummy.foodX + 20) && (myY > yummy.foodX - 20 && myY < yummy.foodY + 20))
- 				eaten = true; //The bacteria has eaten the food
- 			if(eaten == true)
- 			{
- 				colorA = colorB; //The bacteria will become the same color as the food
- 				mousePressed();
- 			}
-}  
-void mousePressed() //Creates a new food with random color and location 
-{
-	yummy = new Food();
-	yummy.show();
-	colorB = color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256));
-	foodsies = true;
-}
-class Food
-{
-	int foodX;
-	int foodY;
-	Food()
-	{
-   		foodX = (int)(Math.random()*450);
-   		foodY = (int)(Math.random()*450);
-    }
-	void show()
-	{
-   		noStroke();
-   		fill(colorB);
-   		rect(foodX,foodY,8,8);
-
-	}
-}
 class Bacteria    
 {   
 	int myX;
 	int myY;
+	int pestColor;
 	Bacteria(int x, int y)
  		{
- 		colorB = color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256));
  		myX = x;
  		myY = y; 
+ 		pestColor = color(150,150,150);
 		}
  	void move()
  	{
@@ -77,7 +21,7 @@ class Bacteria
  	void show()
  	{
  		noStroke();
- 		fill(colorA);
+ 		fill(pestColor);
  		ellipse(myX,myY,15,15);
  	}
  	void chase() //Causes the bacteria to hunt the food
@@ -99,4 +43,60 @@ class Bacteria
  				myY = myY + (int)(Math.random() * 8) - 6;
  		}
  	}
-}   
+} 
+class Food
+{
+	int foodX;
+	int foodY;
+	int foodColor;
+	Food()
+	{
+   		foodX = (int)(Math.random() * 450);
+   		foodY = (int)(Math.random() * 450);
+   		foodColor = color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256)); 
+    }
+	void show()
+	{
+   		noStroke();
+   		fill(foodColor);
+   		rect(foodX,foodY,8,8);
+	}
+}
+void setup()   
+{     
+ 	frameRate(15);
+ 	size(500,500);
+ 	pest = new Bacteria[5];
+ 	for(int index = 0; index < pest.length; index++)
+ 	{
+ 		 pest[index] = new Bacteria(250,250);
+ 	}
+ 	yummy = new Food();
+ }   
+void draw()   
+{   
+	background(255);
+	for(int index = 0; index < pest.length; index++)
+	{
+		pest[index].move();
+ 		pest[index].show();
+ 		pest[index].chase();
+		if((pest[index].myX > yummy.foodX - 10 && pest[index].myX < yummy.foodX + 10) && (pest[index].myY > yummy.foodY - 10 && pest[index].myY < yummy.foodY + 10))
+ 			eaten = true; //The bacteria has eaten the food
+ 		if(eaten == true)
+ 		{
+ 			pest[index].pestColor = yummy.foodColor; //The bacteria will become the same color as the food
+ 			mousePressed();
+ 			eaten = false;
+ 		}
+ 	}
+ 	if(foodsies == true)
+		yummy.show();
+}  
+void mousePressed() //Creates a new food with random color and location 
+{
+	yummy = new Food();
+	yummy.show();
+	foodsies = true;
+}
+  
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -12,78 +12,22 @@ import java.io.InputStream;
 import java.io.OutputStream; 
 import java.io.IOException; 
 
-public class Chemotaxis extends PApplet {
+public class chemotaxis extends PApplet {
 
-
-int colorA = color(150,150,150);
-int colorB;
 boolean eaten = false;
 boolean foodsies = true;
 Bacteria [] pest;
 Food yummy;
-public void setup()   
-{     
- 	frameRate(15);
- 	size(500,500);
- 	pest = new Bacteria[5];
- 	for(int index = 0; index < pest.length; index++)
- 	{
- 		 pest[index] = new Bacteria(250,250);
- 	}
- 	yummy = new Food();
- }   
-public void draw()   
-{   
-	background(255);
-	if(foodsies == true)
-		yummy.show();
-	for(int index = 0; index < pest.length; index++)
-	{
-		pest[index].move();
- 		pest[index].show();
- 		pest[index].chase();
-	}
-	 			if((myX > yummy.foodX - 20 && myX < yummy.foodX + 20) && (myY > yummy.foodX - 20 && myY < yummy.foodY + 20))
- 				eaten = true; //The bacteria has eaten the food
- 			if(eaten == true)
- 			{
- 				colorA = colorB; //The bacteria will become the same color as the food
- 				mousePressed();
- 			}
-}  
-public void mousePressed() //Creates a new food with random color and location 
-{
-	yummy = new Food();
-	yummy.show();
-	colorB = color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256));
-	foodsies = true;
-}
-class Food
-{
-	int foodX;
-	int foodY;
-	Food()
-	{
-   		foodX = (int)(Math.random()*450);
-   		foodY = (int)(Math.random()*450);
-    }
-	public void show()
-	{
-   		noStroke();
-   		fill(colorB);
-   		rect(foodX,foodY,8,8);
-
-	}
-}
 class Bacteria    
 {   
 	int myX;
 	int myY;
+	int pestColor;
 	Bacteria(int x, int y)
  		{
- 		colorB = color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256));
  		myX = x;
  		myY = y; 
+ 		pestColor = color(150,150,150);
 		}
  	public void move()
  	{
@@ -93,7 +37,7 @@ class Bacteria
  	public void show()
  	{
  		noStroke();
- 		fill(colorA);
+ 		fill(pestColor);
  		ellipse(myX,myY,15,15);
  	}
  	public void chase() //Causes the bacteria to hunt the food
@@ -115,9 +59,65 @@ class Bacteria
  				myY = myY + (int)(Math.random() * 8) - 6;
  		}
  	}
-}   
+} 
+class Food
+{
+	int foodX;
+	int foodY;
+	int foodColor;
+	Food()
+	{
+   		foodX = (int)(Math.random() * 450);
+   		foodY = (int)(Math.random() * 450);
+   		foodColor = color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256)); 
+    }
+	public void show()
+	{
+   		noStroke();
+   		fill(foodColor);
+   		rect(foodX,foodY,8,8);
+	}
+}
+public void setup()   
+{     
+ 	frameRate(15);
+ 	size(500,500);
+ 	pest = new Bacteria[5];
+ 	for(int index = 0; index < pest.length; index++)
+ 	{
+ 		 pest[index] = new Bacteria(250,250);
+ 	}
+ 	yummy = new Food();
+ }   
+public void draw()   
+{   
+	background(255);
+	for(int index = 0; index < pest.length; index++)
+	{
+		pest[index].move();
+ 		pest[index].show();
+ 		pest[index].chase();
+		if((pest[index].myX > yummy.foodX - 10 && pest[index].myX < yummy.foodX + 10) && (pest[index].myY > yummy.foodY - 10 && pest[index].myY < yummy.foodY + 10))
+ 			eaten = true; //The bacteria has eaten the food
+ 		if(eaten == true)
+ 		{
+ 			pest[index].pestColor = yummy.foodColor; //The bacteria will become the same color as the food
+ 			mousePressed();
+ 			eaten = false;
+ 		}
+ 	}
+ 	if(foodsies == true)
+		yummy.show();
+}  
+public void mousePressed() //Creates a new food with random color and location 
+{
+	yummy = new Food();
+	yummy.show();
+	foodsies = true;
+}
+  
   static public void main(String[] passedArgs) {
-    String[] appletArgs = new String[] { "Chemotaxis" };
+    String[] appletArgs = new String[] { "chemotaxis" };
     if (passedArgs != null) {
       PApplet.main(concat(appletArgs, passedArgs));
     } else {
